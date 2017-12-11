06513ca142b72594e5ddbdd200c5efdf674560a7
Leonid Boykov <lordkratos.47@gmail.com>
2017-12-11 23:21:58 +0300

    Updated API handlers
      * API is now compatable with smarthut/agent:0.2.0
      * Added CHANGELOG

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+
+## [0.2.0] - 2017-12-12
+### Added
+- Initial release.
+- Basic API implemented.
+- Docker image build pipeline (multi-stage build).
+
+## [0.0.0] - 2017-09-22
+- Initial release
--- handler/device.go
+++ handler/device.go
@@ -1,12 +1,9 @@
 package handler
 
 import (
-	"bytes"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/render"
@@ -32,44 +29,6 @@ func GetDevice(w http.ResponseWriter, r *http.Request) {
 	render.JSON(w, r, d)
 }
 
-// ListSockets ...
-func ListSockets(w http.ResponseWriter, r *http.Request) {
-	name := chi.URLParam(r, "devicename")
-
-	d, err := model.GetDevice(name)
-	if err != nil {
-		render.JSON(w, r, model.Message{Msg: err.Error()})
-		log.Println(err)
-		return
-	}
-
-	render.JSON(w, r, d.Sockets)
-}
-
-// GetSocket ...
-func GetSocket(w http.ResponseWriter, r *http.Request) {
-	name := chi.URLParam(r, "devicename")
-	d, err := model.GetDevice(name)
-
-	if err != nil {
-		render.JSON(w, r, model.Message{Msg: err.Error()})
-		log.Println(err)
-		return
-	}
-
-	num := chi.URLParam(r, "num")
-	i, err := strconv.Atoi(num)
-	if err != nil {
-		render.JSON(w, r, model.Message{Msg: err.Error()})
-		log.Println(err)
-	}
-
-	if i < 0 || i >= len(d.Sockets) {
-		render.JSON(w, r, model.Message{Msg: fmt.Sprintf("smarthome: there is no socket#%d in %s", i, name)})
-	}
-	render.JSON(w, r, d.Sockets[i])
-}
-
 // SetSocket ...
 func SetSocket(w http.ResponseWriter, r *http.Request) {
 	name := chi.URLParam(r, "devicename")
@@ -81,34 +40,19 @@ func SetSocket(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	num := chi.URLParam(r, "num")
-	i, err := strconv.Atoi(num)
+	resp, err := http.Post(d.Host+"/socket", "application/json; charset=utf-8", r.Body)
 	if err != nil {
 		render.JSON(w, r, model.Message{Msg: err.Error()})
 		log.Println(err)
 		return
 	}
 
-	if i < 0 || i >= len(d.Sockets) {
-		render.JSON(w, r, model.Message{Msg: fmt.Sprintf("smarthome: there is no socket#%d in %s", i, name)})
-	}
-
-	r.ParseForm()
-	body := bytes.NewBufferString(r.Form.Encode())
-	resp, err := http.Post(d.Host+"/"+num, "application/x-www-form-urlencoded", body)
-	if err != nil {
-		render.JSON(w, r, model.Message{Msg: err.Error()})
-		log.Println(err)
-		return
-	}
-	defer resp.Body.Close()
-
-	respBody, err := ioutil.ReadAll(resp.Body)
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		render.JSON(w, r, model.Message{Msg: err.Error()})
 		log.Println(err)
 		return
 	}
 
-	render.JSON(w, r, string(respBody))
+	render.JSON(w, r, string(body))
 }
