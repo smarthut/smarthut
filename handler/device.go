b6d187702fe1f34700570e715d7778aca03fc496
Leonid Boykov <lordkratos.47@gmail.com>
2018-04-20 21:33:34 +0300

    fixed: windows build

--- handler/device.go
+++ handler/device.go
@@ -1,11 +1,8 @@
 package handler
 
 import (
-	"io/ioutil"
-	"log"
 	"net/http"
 
-	"github.com/go-chi/chi"
 	"github.com/go-chi/render"
 	"github.com/smarthut/smarthut/model"
 )
@@ -15,44 +12,44 @@ func ListDevices(w http.ResponseWriter, r *http.Request) {
 	render.JSON(w, r, model.ListDevices())
 }
 
-// GetDevice ...
-func GetDevice(w http.ResponseWriter, r *http.Request) {
-	name := chi.URLParam(r, "devicename")
+// // GetDevice ...
+// func GetDevice(w http.ResponseWriter, r *http.Request) {
+// 	name := chi.URLParam(r, "devicename")
 
-	d, err := model.GetDevice(name)
-	if err != nil {
-		render.JSON(w, r, model.Message{Msg: err.Error()})
-		log.Println(err)
-		return
-	}
+// 	d, err := model.GetDevice(name)
+// 	if err != nil {
+// 		render.JSON(w, r, model.Message{Msg: err.Error()})
+// 		log.Println(err)
+// 		return
+// 	}
 
-	render.JSON(w, r, d)
-}
+// 	render.JSON(w, r, d)
+// }
 
-// SetSocket ...
-func SetSocket(w http.ResponseWriter, r *http.Request) {
-	name := chi.URLParam(r, "devicename")
-	d, err := model.GetDevice(name)
+// // SetSocket ...
+// func SetSocket(w http.ResponseWriter, r *http.Request) {
+// 	name := chi.URLParam(r, "devicename")
+// 	d, err := model.GetDevice(name)
 
-	if err != nil {
-		render.JSON(w, r, model.Message{Msg: err.Error()})
-		log.Println(err)
-		return
-	}
+// 	if err != nil {
+// 		render.JSON(w, r, model.Message{Msg: err.Error()})
+// 		log.Println(err)
+// 		return
+// 	}
 
-	resp, err := http.Post(d.Host+"/api/v1/socket", "application/json; charset=utf-8", r.Body)
-	if err != nil {
-		render.JSON(w, r, model.Message{Msg: err.Error()})
-		log.Println(err)
-		return
-	}
+// 	resp, err := http.Post(d.Host+"/api/v1/socket", "application/json; charset=utf-8", r.Body)
+// 	if err != nil {
+// 		render.JSON(w, r, model.Message{Msg: err.Error()})
+// 		log.Println(err)
+// 		return
+// 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		render.JSON(w, r, model.Message{Msg: err.Error()})
-		log.Println(err)
-		return
-	}
+// 	body, err := ioutil.ReadAll(resp.Body)
+// 	if err != nil {
+// 		render.JSON(w, r, model.Message{Msg: err.Error()})
+// 		log.Println(err)
+// 		return
+// 	}
 
-	render.JSON(w, r, string(body))
-}
+// 	render.JSON(w, r, string(body))
+// }
