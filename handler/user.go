8dbecb47ee2cc2c63f4cdfccedf3550386f966fe
Leonid Boykov <lordkratos.47@gmail.com>
2018-04-20 21:44:59 +0300

    revert changes

--- handler/user.go
+++ handler/user.go
@@ -1,29 +1,39 @@
 package handler
 
-// type omit bool
+import (
+	"log"
+	"net/http"
 
-// // ListUsers all users
-// func ListUsers(w http.ResponseWriter, r *http.Request) {
-// 	render.JSON(w, r, model.ListUsers())
-// }
+	"github.com/go-chi/chi"
+	"github.com/go-chi/render"
 
-// // GetUser returns user
-// func GetUser(w http.ResponseWriter, r *http.Request) {
-// 	login := chi.URLParam(r, "username")
+	"github.com/smarthut/smarthut/model"
+)
 
-// 	u, err := model.GetUser(login)
-// 	if err != nil {
-// 		render.JSON(w, r, model.Message{Msg: err.Error()})
-// 		log.Println(err)
-// 		return
-// 	}
+type omit bool
 
-// 	render.JSON(w, r, &struct {
-// 		*model.User
-// 		ID       string `json:"id"`
-// 		Password omit   `json:"password,omitempty"`
-// 	}{
-// 		User: &u,
-// 		ID:   login,
-// 	})
-// }
+// ListUsers all users
+func ListUsers(w http.ResponseWriter, r *http.Request) {
+	render.JSON(w, r, model.ListUsers())
+}
+
+// GetUser returns user
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	login := chi.URLParam(r, "username")
+
+	u, err := model.GetUser(login)
+	if err != nil {
+		render.JSON(w, r, model.Message{Msg: err.Error()})
+		log.Println(err)
+		return
+	}
+
+	render.JSON(w, r, &struct {
+		*model.User
+		ID       string `json:"id"`
+		Password omit   `json:"password,omitempty"`
+	}{
+		User: &u,
+		ID:   login,
+	})
+}
